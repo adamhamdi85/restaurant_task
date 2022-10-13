41be87604f13f5befc1ca81afe68fe7fe31156eb
adamhamdi85 <adamhamdi85@gmail.com>
2022-10-13 10:22:13 +0100

    restaurant task

---
+++ restaurant_task.py
@@ -0,0 +1,12 @@
+# Restaurant task
+
+menu = ["chicken", "pasta", "meat", "juice", "tomato" ]
+print("In the menu, having {}, {}, {}, {}, {}".format(menu[0], menu[1], menu[2], menu[3],menu[4]))
+order_list = []
+order = input("What is it you would like to order?")
+order_list.append(order)
+order = input("Anything else you would like to order?")
+order_list.append(order)
+order = input("Anything else you would like to order?")
+order_list.append(order)
+order_list = 3
